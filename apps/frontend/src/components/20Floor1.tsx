c1649553284c2b08d3eba6353368c1ff8dcfc7e7
AlexLowczyk <alex.lowczyk@gmail.com>
2025-04-01 19:15:39 -0400

    Floor Lists

---
+++ apps/frontend/src/components/20Floor1.tsx
@@ -0,0 +1,13 @@
+export function TwentyFloorOne(){
+    return(
+        <ul>
+            <li>Blood Draw/Phlebotomy</li>
+            <li>Pharmacy</li>
+            <li>Radiology</li>
+            <li>Cardiovascular Services</li>
+            <li>Urology</li>
+            <li>Urgent Care Center</li>
+        </ul>
+    )
+}
+export default TwentyFloorOne;
