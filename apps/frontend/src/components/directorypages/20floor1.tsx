867093511891684133a564543a0bdb161aa97335
Keagan Hitt <kjhitt@wpi.edu>
2025-04-01 10:34:36 -0400

    changed base files

---
+++ apps/frontend/src/components/directorypages/20floor1.tsx
@@ -0,0 +1,20 @@
+export function Floor20_1() {
+    return (
+        <div>
+            <ol>
+                <li>Item 1</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+                <li>
+                    <ol>
+                        <li>Item 1</li>
+                        <li>Item 1</li>
+                        <li>Item 1</li>
+                        <li>Item 1</li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
+    )
+}
