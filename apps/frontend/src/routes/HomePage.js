32057af475d7fde994b0ebbe3d667a2e69a63a1c
Brian Grande <101441595+B-grande@users.noreply.github.com>
2025-04-03 16:26:49 -0500

    Delete apps/frontend/src/routes/HomePage.js

--- apps/frontend/src/routes/HomePage.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import React from 'react';
-import ExampleComponent from "../components/ExampleComponent.tsx";
-import HomeHeader from "../components/HomeHeader";
-var HomePage = function () {
-    return (_jsxs("div", { className: "p-10", children: [_jsx(HomeHeader, { title: "Home", homelabel: "home test.", homelabelLink: "/test", formlabel: "Service Request", formlabelLink: "/servicereqs" }), _jsx("h1", { className: "font-bold text-xl pb-4", children: "Home Page" }), _jsx(ExampleComponent, {})] }));
-};
-export default HomePage;
