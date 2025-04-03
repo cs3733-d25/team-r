ca6591983858506e5a3a42bb827afdd46e77b8f6
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2025-04-03 17:16:51 -0400

    console.log for employees, assigned employees, and device requests. deleted score router

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -1,6 +1,5 @@
 export const API_ROUTES = {
     HEALTHCHECK: '/api/healthcheck',
-    SCORE: '/api/score',
     EMPLOYEE: '/api/employee',
     SERVICE: '/api/servicereq',
     ASSIGNED: '/api/assigned',
