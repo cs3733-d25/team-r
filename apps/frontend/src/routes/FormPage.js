7f2c703913c2ab022c00063064e5c201db0ce89d
Brian Grande <101441595+B-grande@users.noreply.github.com>
2025-04-03 16:26:40 -0500

    Delete apps/frontend/src/routes/FormPage.js

--- apps/frontend/src/routes/FormPage.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import DeviceReqForm from "../components/DeviceReqForm.tsx";
-var FormPage = function () {
-    return (_jsx("div", { className: "p-10", children: _jsx(DeviceReqForm, {}) }));
-};
-export default FormPage;
