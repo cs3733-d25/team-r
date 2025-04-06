8280f5f94c9c2134502c9794c42aea3a48f160f6
Akaash-Walker <awalker7179@gmail.com>
2025-04-06 15:53:48 -0400

    clean: remove HomePage.tsx and replaced it with HomeMain

--- apps/frontend/src/routes/App.tsx
+++ apps/frontend/src/routes/App.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import FormPage from '../features/MedDeviceRequest/FormPage.tsx';
 import Directory from '../features/Directory/Directory.tsx';
-import Login from "../features/Login/Login.tsx";
+import Login from '../features/Login/Login.tsx';
 import MapView from '../features/MapView/MapView.tsx';
-import SanitationForm from "../features/SanitationForm/SanitationForm.tsx";
-import {HomeMain} from "../components/HomeMain.tsx";
-
-
+import SanitationForm from '../features/SanitationForm/SanitationForm.tsx';
+import { HomeMain } from '../components/HomeMain.tsx';
 
 function App() {
     const router = createBrowserRouter([
@@ -21,9 +19,9 @@ function App() {
                 { path: 'servicereqs', element: <FormPage /> },
                 { path: 'directory', element: <Directory /> },
                 { path: 'mapView', element: <MapView /> },
-                { path: 'sanitationFrom' , element: <SanitationForm /> },
+                { path: 'sanitationFrom', element: <SanitationForm /> },
             ],
-        }
+        },
     ]);
 
     return <RouterProvider router={router} />;
