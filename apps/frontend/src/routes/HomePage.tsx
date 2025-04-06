dcb9f7c853becc8b9e0a56312f95d6321f010e7f
Keagan Hitt <kjhitt@wpi.edu>
2025-04-06 10:43:07 -0400

    added HomeMain as a component for HomePage and added some text for the hero page

--- apps/frontend/src/routes/HomePage.tsx
+++ apps/frontend/src/routes/HomePage.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import NavBar from "../components/NavBar.tsx";
+import {HomeMain} from "../components/HomeMain.tsx";
 
 
 const HomePage = () => {
     return (
-        <div className={"bg-[#449aa4] flex-col h-screen"}>
-            <NavBar page={"login"} />
-            {/*Image taken from Mass General Brigham website home page*/}
-            <div className={"bg-[url(/heropage.jpeg)] bg-no-repeat bg-cover h-6/7"}>
-
-            </div>
-        </div>
+        <HomeMain />
     );
 };
 
