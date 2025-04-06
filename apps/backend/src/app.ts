0e7f011b48beb51293fd9075c0df734f7edb9f77
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2025-04-06 16:13:06 -0400

    api route for login page

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -6,6 +6,7 @@ import healthcheckRouter from "./routes/healthcheck";
 import employeeRouter from "./routes/employee.ts";
 import servicereqRouter from "./routes/servicereqs.ts";
 import assignedRouter from "./routes/assigned.ts";
+import loginRouter from "./routes/login.ts";
 import { API_ROUTES } from "common/src/constants";
 
 const app: Express = express(); // Setup the backend
@@ -30,6 +31,7 @@ app.use(API_ROUTES.HEALTHCHECK, healthcheckRouter);
 app.use(API_ROUTES.EMPLOYEE, employeeRouter);
 app.use(API_ROUTES.SERVICE, servicereqRouter);
 app.use(API_ROUTES.ASSIGNED, assignedRouter);
+app.use(API_ROUTES.LOGIN, loginRouter);
 
 /**
  * Catch all 404 errors, and forward them to the error handler
--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -3,4 +3,5 @@ export const API_ROUTES = {
     EMPLOYEE: '/api/employee',
     SERVICE: '/api/servicereq',
     ASSIGNED: '/api/assigned',
+    LOGIN: '/api/login'
 };
