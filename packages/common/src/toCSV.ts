7ffbb9d755f067adc9d23066158c5580eee7060a
sarayuv <sarayu.vijayanagaram@outlook.com>
2025-04-06 23:23:54 -0400

    added export function

--- packages/common/src/toCSV.ts
+++ packages/common/src/toCSV.ts
@@ -3,7 +3,7 @@
  * @param {Object[]} data - the array of objects to convert into CSV format
  * @returns {string} - the CSV string representation of the input data
  */
-function toCSV(data: any[]): string {
+export function toCSV(data: any[]): string {
     if (data.length === 0) return '';
 
     // get headers from keys of the first object
