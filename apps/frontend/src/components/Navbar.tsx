a388c74ff7558fa400320a8be277eddadfcdcae1
Keagan Hitt <kjhitt@wpi.edu>
2025-04-08 14:47:45 -0400

    has links displayed differently on mobile, not formatted & no collapse

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -15,6 +15,50 @@ function Navbar(props: NavBarProps) {
         { name: 'Map', path: '/mapView' },
     ];
 
+    function loginButton() {
+        return(
+            <Link
+                key={'Login'}
+                to={'/login'}
+                className={'px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 text-black bg-[#b8e4e4] hover:bg-[#306c73] hover:text-white'}
+            >
+                {'Login'}
+            </Link>
+        )
+    }
+
+    function desktopLinks() {
+        return (
+            navigationLinks.map((link) => (
+                <Link
+                    key={link.path}
+                    to={link.path}
+                    className={`px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ${
+                        location.pathname === link.path
+                            ? /* Again i don't like using a manual hex code but i have no
+                                               * other option at atm*/
+                            'bg-[#234d52] text-white'
+                            : 'text-white hover:bg-[#306c73] hover:text-white'
+                    }`}
+                >
+                    {link.name}
+                </Link>
+            ))
+        )
+    }
+
+    function mobileLinks() {
+        return (
+            navigationLinks.map((link) => (
+                <Link key={link.path}
+                      to={link.path}
+                      className={``}>
+                    {link.name}
+                </Link>
+            ))
+        )
+    }
+
     return (
         /*NEED TO FIGURE OUT TAILWIND CONFIG ASAP
          * these colors should be added to it instead
@@ -34,32 +78,22 @@ function Navbar(props: NavBarProps) {
                     <div className="hidden md:block">
                         <div className="ml-10 flex items-center space-x-4">
                             {props.page == 'login' ? (
-                                <Link
-                                    key={'Login'}
-                                    to={'/login'}
-                                    className={'px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 text-black bg-[#b8e4e4] hover:bg-[#306c73] hover:text-white'}
-                                >
-                                    {'Login'}
-                                </Link>
+                                loginButton()
                             ) : (
-                                navigationLinks.map((link) => (
-                                <Link
-                                    key={link.path}
-                                    to={link.path}
-                                    className={`px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ${
-                                        location.pathname === link.path
-                                            ? /* Again i don't like using a manual hex code but i have no
-                                               * other option at atm*/
-                                              'bg-[#234d52] text-white'
-                                            : 'text-white hover:bg-[#306c73] hover:text-white'
-                                    }`}
-                                >
-                                    {link.name}
-                                </Link>
-                            ))
+                                desktopLinks()
                             )}
                         </div>
                     </div>
+                    <div className={"md:hidden"}>
+                        <button data-toggle="collapse" data-target="#hamburg" type="button" className={"inline-flex items-center justify-center p-2 bg-gray-500"}>
+                            a
+                        </button>
+                    </div>
+                </div>
+                <div id={"hamburg"} className={"md:hidden"}>
+                    <ul className={"flex flex-col"}>
+                        {mobileLinks()}
+                    </ul>
                 </div>
             </div>
         </nav>
