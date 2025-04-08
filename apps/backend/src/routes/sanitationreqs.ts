f49fc3ae8be030a37596f7c6b7e3ff5785f56dc5
O-bot22 <owenrmiller@gmail.com>
2025-04-08 14:14:17 -0400

    feature: add support for users to be linked to sanitation requests

--- apps/backend/src/routes/sanitationreqs.ts
+++ apps/backend/src/routes/sanitationreqs.ts
@@ -1,5 +1,7 @@
 import express, { Router, Request, Response } from "express";
 import PrismaClient from "../bin/prisma-client.ts";
+import { Prisma } from "../../../../packages/database";
+import PrismaClientValidationError = Prisma.PrismaClientValidationError;
 
 const router: Router = express.Router();
 
@@ -17,18 +19,30 @@ router.get("/", async function (req: Request, res: Response) {
 router.post("/", async function (req: Request, res: Response) {
   console.log("A user entered a sanitation request");
   const request = req.body;
-  console.log("Room: " + request.room);
+  console.log("Room: " + request.roomNumber);
   try {
     // assumes that the request is formatted with the exact fields as the SanitationRequest table in the prisma schema (packages/database/prisma/schema.prisma)
     const createRequest = await PrismaClient.sanitationRequest.create({
-      data: request,
+      data: {
+        sanitationType: request.sanitationType,
+        priority: request.priority,
+        department: request.department,
+        roomNumber: request.roomNumber,
+        comments: request.comments,
+        user: { connect: { id: request.userID } }, // connect to whatever user has that ID number
+      },
     });
     // console.log(createRequest);
     res
       .status(200)
       .json({ message: "Successfully entered sanitation request" });
   } catch (error) {
-    console.error("Error entering service request data:", error);
+    if (error instanceof PrismaClientValidationError) {
+      console.log(error);
+      console.log("that user may not exist");
+    } else {
+      console.error("Error entering service request data:", error);
+    }
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
