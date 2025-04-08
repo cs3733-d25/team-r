6740e83cd88b28073a72227419807d3df8888b8d
B-grande <101441595+B-grande@users.noreply.github.com>
2025-04-08 14:43:22 -0400

    added iteration one stuff to my part hopefully

--- apps/backend/src/routes/csv.ts
+++ apps/backend/src/routes/csv.ts
@@ -9,44 +9,44 @@ const router: Router = express.Router();
 
 // export directory data as CSV
 router.get("/export", async (req: Request, res: Response) => {
-    try {
-        const directoryData = await PrismaClient.directory.findMany();
-        const csv = toCSV(directoryData);
+  try {
+    const directoryData = await PrismaClient.directory.findMany();
+    const csv = toCSV(directoryData);
 
-        // set response headers
-        res.setHeader("Content-Type", "text/csv");
-        res.setHeader("Content-Disposition", "attachment; filename=directory.csv");
-        res.status(200).send(csv);
-    } catch (error) {
-        console.error("Error exporting directory data:", error);
-        res.sendStatus(500)
-    }
+    // set response headers
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment; filename=directory.csv");
+    res.status(200).send(csv);
+  } catch (error) {
+    console.error("Error exporting directory data:", error);
+    res.sendStatus(500);
+  }
 });
 
 // import directory from CSV
 router.post("/import", async (req: Request, res: Response) => {
-    try {
-        const absolutePath = path.join(__dirname, "../../data/csv/Directory.csv");
-        const csvFile = fs.readFileSync(absolutePath, "utf8");
+  try {
+    const absolutePath = path.join(__dirname, "../../data/csv/Directory.csv");
+    const csvFile = fs.readFileSync(absolutePath, "utf8");
 
-        const records = parseCSV(csvFile);
+    const records = parseCSV(csvFile);
 
-        const transformation = records.map((row) => ({
-            id: parseInt(row.ID),
-            name: row.Name,
-            type: row.Type,
-            location: row.Location
-        }));
+    const transformation = records.map((row) => ({
+      id: parseInt(row.ID),
+      name: row.Name,
+      type: row.Type,
+      location: row.Location,
+    }));
 
-        await PrismaClient.directory.createMany({
-            data: transformation
-        });
+    await PrismaClient.directory.createMany({
+      data: transformation,
+    });
 
-        res.sendStatus(200);
-    } catch (error) {
-        console.error("Error importing directory data:", error);
-        res.sendStatus(500);
-    }
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error importing directory data:", error);
+    res.sendStatus(500);
+  }
 });
 
 export default router;
--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import ExamplePage from './routes/ExamplePage.tsx';
-import SanitationRequestForm from './components/SanitationRequestForm.tsx';
+import SanitationRequestForm from './components/SanitationRequest/SanitationRequestForm.tsx';
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: '/',
-            errorElement: <div />,
-            element: <SanitationRequestForm  />,
-        },
-    ]);
+  const router = createBrowserRouter([
+      {
+          path: '/',
+          errorElement: <p>Page not found</p>,
+          children: [
+              { index: true, element: <HomeMain /> },
+              { path: 'login', element: <Login /> },
+              { path: 'servicereqs', element: <FormPage /> },
+              { path: 'directory', element: <Directory /> },
+              { path: 'mapView', element: <MapView /> },
+              { path: 'sanitation', element: <SanitationRequestForm /> }, // Add this line
+          ],
+      },
+  ]);
 
     return <RouterProvider router={router} />;
 }
