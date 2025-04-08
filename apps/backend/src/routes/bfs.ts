8d0de109da29dde0b779dbc88fc7c128da550d40
dgajaria11 <dakshgajaria@gmail.com>
2025-04-08 12:57:02 -0400

    pushing 1pm tuesday: starting class based apporach bfs

--- apps/backend/src/routes/bfs.ts
+++ apps/backend/src/routes/bfs.ts
@@ -3,35 +3,87 @@ import express, { Router, Request, Response } from "express";
 import PrismaClient from "../bin/prisma-client.ts";
 const router: Router = express.Router();
 
-router.get("/", async function (req: Request, res: Response) {
+interface NodeData {
+    id: string;
+    name: string;
+    type: string;
+}
+
+class Node implements NodeData {
+    id: string;
+    name: string;
+    neighbors: string[] = [];
+
+    constructor(data: NodeData) {
+        this.id = data.id;
+        this.name = data.name;
+    }
+
+    addNeighbor(neighborId: string): void {
+        this.neighbors.push(neighborId);
+    }
+}
+
+class Graph {
+    private nodes: Node[] = [];
+    private nodeIndex: number = 0;
+    private adjacencyList: Node[] = [];
+    private adjacentIndex: number = 0;
+
+    addNode(nodeData: NodeData): Boolean {
+        if (this.nodeIndex < this.nodes.length) {
+            nodes[nodeIndex] = nodeData;
+            nodeIndex++;
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    addEdge(node1Id: string, node2Id: string): Boolean {}
+
+    getNeighbors(nodeId: String): string[] {}
+}
+
+
+
+
+class Pathfinder{
+    private graph: Graph;
+
+    constructor(graph: Graph) {
+        this.graph = graph;
+    }
+
+    BFSShortestPath(start: string, end: string): string {
+
+    }
+}
+
+
+const hospitalGraph = new Graph();
+// add all hospital node data and outside data
+hospitalGraph.addNode({id: 'parking21324', name: 'Parking A', type: 'parking'});
+hospitalGraph.addEdge({})
+
+const pathFinder = new Pathfinder(hospitalGraph);
+
+router.post("/", async function (req: Request, res: Response) {
+    const {startingPoint, endingPoint} = req.body;
     console.log("hello algorithm");
 
     try {
-        const employee = await PrismaClient.employee.findMany();
-        console.log(employee);
-        res.status(200).json(employee); // Send employee data as JSON
+        const pf = new Pathfinder();
+        //console.log(e);
+        const path = pf.findPath(startingPoint, endingPoint);
+        res.status(200).json(path); // Send path data as JSON
     } catch (error) {
-        console.error("Error fetching employee:", error);
+        console.error("Pathfinding:", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
-
-//hahaha
-
-
-//client server communication
-
-//import in app.ts
-//step routers (app.use)
-
-
-
-struct Node {
-
-}
-
-//updates constants.ts
-
 
 
 export default router;
