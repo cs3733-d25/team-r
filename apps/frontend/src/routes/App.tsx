f84603fddfb7189ca5ebf975cce7b546a83cda26
Akaash-Walker <awalker7179@gmail.com>
2025-04-13 01:31:28 -0400

    styling: added shadcn components

--- apps/frontend/src/routes/App.tsx
+++ apps/frontend/src/routes/App.tsx
@@ -7,9 +7,9 @@ import Login from '../features/Login/Login.tsx';
 import MapView from '../features/MapView/MapView.tsx';
 import SanitationRequestForm from '../features/SanitationForm/SanitationRequestForm.tsx';
 import { HomeMain } from '../components/HomeMain.tsx';
-import CSVPage from "../features/CSVFiles/CSVPage.tsx";
-import SanitationRequestPage from "../features/SanitationForm/SanitationRequestPage.tsx";
-
+import CSVPage from '../features/CSVFiles/CSVPage.tsx';
+import SanitationRequestPage from '../features/SanitationForm/SanitationRequestPage.tsx';
+import TestPage from '../features/TestPage.tsx';
 
 function App() {
     const router = createBrowserRouter([
@@ -18,14 +18,15 @@ function App() {
             errorElement: <p>Page not found</p>,
             children: [
                 { index: true, element: <HomeMain /> },
-                { path: 'home', element: <HomeMain status={"logged-in"} /> },
+                { path: 'home', element: <HomeMain status={'logged-in'} /> },
                 { path: 'login', element: <Login /> },
                 { path: 'servicereqs', element: <FormPage /> },
                 { path: 'directory', element: <Directory /> },
                 { path: 'mapView', element: <MapView /> },
                 { path: 'sanitation', element: <SanitationRequestForm /> },
-                {path: 'csv', element:<CSVPage/>},
-                {path: 'sanitationpage', element: <SanitationRequestPage />},
+                { path: 'csv', element: <CSVPage /> },
+                { path: 'sanitationpage', element: <SanitationRequestPage /> },
+                { path: "testing", element: <TestPage/> }
             ],
         },
     ]);
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
-import tailwindcss from '@tailwindcss/vite';
 import * as process from 'process';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
         preserveSymlinks: true,
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
     server: {
         host: 'localhost',
