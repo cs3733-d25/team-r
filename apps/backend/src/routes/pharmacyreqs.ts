345342c7f217a8a321b4183693d0a69557e15f36
O-bot22 <owenrmiller@gmail.com>
2025-04-14 13:24:13 -0400

    feature: use enums backend in pharmacy request

--- apps/backend/src/routes/pharmacyreqs.ts
+++ apps/backend/src/routes/pharmacyreqs.ts
@@ -1,6 +1,7 @@
-import express, { Router, Request, Response } from "express";
+import express, { Request, Response, Router } from "express";
 import PrismaClient from "../bin/prisma-client.ts";
-import { Prisma, RequestPriority, Department } from "database";
+import { Prisma } from "database";
+import { parseDepartment, parseRequestPriority} from "./enum.ts";
 import PrismaClientValidationError = Prisma.PrismaClientValidationError;
 
 const router: Router = express.Router();
@@ -18,57 +19,16 @@ router.get("/all-requests", async function (req: Request, res: Response) {
   }
 });
 
-router.get("/departments", async function (req: Request, res: Response) {
-  try {
-    console.log("department types requested");
-    // format departments as an array of strings, and then return it to the client
-
-    // res.status(200).json(Object.values(Department)); // uglier way to do it
-
-    // query the database to get an array of rows
-    let departmentList = await PrismaClient.departments
-      .findMany({
-        select: {
-          departmentName: true,
-        },
-      })
-      .then((rows) => {
-        // parse the names out of their rows
-        return rows.map((row) => {
-          return row.departmentName;
-        });
-      });
-
-    // return the rows
-    res.status(200).json(departmentList);
-  } catch (error) {
-    console.error("Error fetching department data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// a function to cast a string to a RequestPriority enum type
-function parseRequestPriority(value: string): RequestPriority {
-  return RequestPriority[value as keyof typeof RequestPriority];
-}
-// a function to cast a string to a Department enum type
-function parseDepartment(value: string): Department {
-  return Department[value as keyof typeof Department];
-}
-
 router.post("/", async function (req: Request, res: Response) {
   console.log("A user entered a pharmacy request");
   const request = req.body;
-  console.log(request);
+  // console.log(request);
   try {
-    // assumes that the request is formatted with the exact fields as the SanitationRequest table in the prisma schema (packages/database/prisma/schema.prisma)
-    console.log(parseDepartment(request.department));
-    console.log(parseRequestPriority(request.priority));
     await PrismaClient.pharmacyRequest.create({
       data: {
         employee: { connect: { id: request.employeeID } }, // connect to whatever employee has that ID number
         priority: parseRequestPriority(request.priority),
-        department: parseDepartment(request.department),
+        department: await parseDepartment(request.department),
         patient: { connect: { id: request.patientID } }, // connect to whatever patient has that ID number
         drugName: request.drugName,
         morningPillCount: request.morningPillCount,
