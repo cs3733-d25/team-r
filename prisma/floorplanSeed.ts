fe6273b62b84ed44941f97d182d0580ecf4a9d8a
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2025-04-13 23:12:09 -0400

    refactor: seed file for fourth floor nodes

--- prisma/floorplanSeed.ts
+++ prisma/floorplanSeed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../packages/database";
+import {PrismaClient} from "../packages/database";
 import { NodeType, Building } from "../packages/database";
 
 const prisma = new PrismaClient();
@@ -14,6 +14,27 @@ async function main() {
 
 
     // patriot 22 floor 4
+    await prisma.node.createMany({
+        data: [
+            { nodeID: 'rightEntrance4', nodeType: NodeType.enterance, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'rightEntranceFourth'},
+            { nodeID: 'checkIn4000B', nodeType: NodeType.reception, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'rightCheckIn'},
+            { nodeID: 'waiting4000', nodeType: NodeType.other, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'rightEntranceWaiting'},
+            { nodeID: 'rightWaitingHallway', nodeType: NodeType.hallway, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'rightWaitingHallway'},
+            { nodeID: 'elevatorLobby4A', nodeType: NodeType.elevator, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'elevatorLobby'},
+            { nodeID: 'lobbyStairsHallway', nodeType: NodeType.stairs, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'lobbyStairsHallway'},
+            { nodeID: 'stairST01', nodeType: NodeType.stairs, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'lobbyStairs'},
+            { nodeID: 'stairST02', nodeType: NodeType.stairs, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'leftStairs'},
+            { nodeID: 'corridor4R', nodeType: NodeType.hallway, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'checkOutHallway'},
+            { nodeID: 'checkOutWait4083', nodeType: NodeType.other, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'checkOutWait'},
+            { nodeID: 'checkOut4090', nodeType: NodeType.other, building: Building.PATRIOT_PLACE_22, floor: "4", xcoord: null, ycoord: null, longName: null, shortName: 'checkOut'},
+        ]
+    });
+
+    await prisma.edge.createMany({
+        data: [
+            { fromID:'rightEntrance4', toID:'checkIn4000B', fromX: null, fromY: null, toX: null, toY: null },
+        ]
+    });
 
 
     console.log('Database seeded successfully!');
