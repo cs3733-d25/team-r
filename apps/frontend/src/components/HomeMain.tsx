1e93aae5b8fcc2582cdaf1a8d3ef1d70531d11b7
Keagan Hitt <kjhitt@wpi.edu>
2025-04-15 19:23:09 -0400

    styling: changed hero page a little bit when logged in, added two more images

--- apps/frontend/src/components/HomeMain.tsx
+++ apps/frontend/src/components/HomeMain.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from "./Navbar.tsx";
+import {NavbarMGH} from "@/components/NavbarMGH.tsx";
 
 interface HomeMainProps {
     status?: string;
@@ -7,15 +8,15 @@ interface HomeMainProps {
 
 export function HomeMain(props: HomeMainProps) {
     return (
-        <div className={"bg-primary flex-col h-screen"}>
+        <div className={"bg-primary h-screen"}>
             {props.status == 'logged-in' ? (
-                <NavBar />
+                <NavbarMGH />
             ) : (<NavBar page={"home"} />)}
             {/*Image taken from Mass General Brigham website home page*/}
-            <div className={"bg-[url(/heropage.jpeg)] bg-no-repeat bg-cover h-6/7 justify-items-center"}>
-                <div className={"flex-col text-center w-5/8 bg-background/80"}>
+            <div className={"flex bg-[url(/hero-page-3.jpeg)] bg-no-repeat bg-cover h-6/7 items-center"}>
+                <div className={"text-center w-3/8 bg-background/80"}>
                     <br />
-                    <h1 className={"text-5xl font-bold"}>Welcome to Mass General Brigham</h1>
+                    <h1 className={"text-5xl place-self-center"}>Welcome to Mass General Brigham</h1>
                     <br />
                     {props.status != 'logged-in' && (
                         <div>
