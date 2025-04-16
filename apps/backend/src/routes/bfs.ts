49dcc07f6b2d3397cf601084ad5531e696fe9d2f
dgajaria11 <dakshgajaria@gmail.com>
2025-04-15 21:21:29 -0400

    algo changes with get routing

--- apps/backend/src/routes/bfs.ts
+++ apps/backend/src/routes/bfs.ts
@@ -1,25 +1,28 @@
 import express, { Router, Request, Response } from "express";
 import PrismaClient from "../bin/prisma-client.ts";
-const prisma = new PrismaClient();
 const router: Router = express.Router();
+const prisma = new PrismaClient();
 
-interface NodeData {
-  id: string;
-  name: string;
-  type: string;
-  xPos: number;
-  yPos: number;
-}
+
 
   //does the Breadth First Search (BFS)
   async function BFS(start: string, end: string): Promise<string[]> {
     const visited = new Set<string>(); //visited list
     const queue: string[][] = [[start]]; //where to go next
 
-    const startNode = await PrismaClient.Node.findMany({
-      where: {
-        
-    });
+    //const nodes = await prisma.nodes.findMany()
+    const edges = await prisma.edge.findMany();
+
+    const graph = new Map<string, Set<string>>();
+
+    for (const edge of edges) {
+      if (!graph.has(edge.fromNodeID)) graph.set(edge.fromNodeID, new Set());
+      if (!graph.has(edge.toNodeID)) graph.set(edge.toNodeID, new Set());
+
+      graph.get(edge.fromNodeID)!.add(edge.toNodeID);
+      graph.get(edge.toNodeID)!.add(edge.fromNodeID);
+    }
+
     while (queue.length > 0) {
       //while we have nodes to visit
       const path = queue.shift();
@@ -35,7 +38,7 @@ interface NodeData {
       if (!visited.has(currentID)) {
         //have we been here? If not...
         visited.add(currentID); //add to visited list
-        const neighbors = graph.get(currentID);
+        const neighbors = graph.get(currentID) || new Set();
 
         for (const neighborId of neighbors) {
           if (!visited.has(neighborId)) {
@@ -47,7 +50,7 @@ interface NodeData {
 
     return []; // No path found
   }
-}
+
 
 //send data to front end
 router.post("/", async function (req: Request, res: Response) {
@@ -55,9 +58,10 @@ router.post("/", async function (req: Request, res: Response) {
   console.log("Starting BFS algorithm");
 
   try {
-    const pf = BFS(startingPoint, endingPoint);
-    if (pf.length > 0) {
-      res.status(200).json(pf);
+    const path = await BFS(startingPoint, endingPoint);
+    //const pf = BFS(startingPoint, endingPoint);
+    if (path.length > 0) {
+      res.status(200).json(path);
     } else {
       res.status(404).json({
         message:
@@ -75,4 +79,20 @@ router.post("/", async function (req: Request, res: Response) {
   }
 });
 
+router.get("/full", async (req: Request, res: Response) => {
+  try {
+    const nodes: Node[] = await prisma.node.findMany();
+    const edges: Edge[] = await prisma.edge.findMany();
+
+    res.json({
+      nodes,
+      edges
+    });
+  } catch (error) {
+    console.error("Failed to load full graph:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 export default router;
+
