620aabb9896616319fc8e12995138065fb154e45
Keagan Hitt <kjhitt@wpi.edu>
2025-04-15 21:22:44 -0400

    styling: changed fonts

--- apps/frontend/src/features/Requests/MedDeviceRequest/DeviceReqPage.tsx
+++ apps/frontend/src/features/Requests/MedDeviceRequest/DeviceReqPage.tsx
@@ -38,7 +38,7 @@ export function DeviceReqPage() {
 
     return (
         <div className={"bg-white"}>
-            <h1 className = {"bold text-3xl text-center pb-2"}>Device Requests</h1>
+            <h1 className = {"bold text-3xl font-trade text-center pb-2"}>Device Requests</h1>
             <Table className = {"mx-auto w-200"}>
                 <TableHeader className = {"border-b"}>
                 <TableRow className={'text-lg border-b'}>
