9ede046fb72ae80899f26e6b8892466fdfc9c9df
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2025-04-21 14:07:02 -0400

    refactor:Patriot Place 22 Floors 3, 4, Patriot Place 20 floor 1 departments/reception node seed

--- prisma/departmentSeed.ts
+++ prisma/departmentSeed.ts
@@ -18,11 +18,61 @@ async function main() {
             {id: 'Multi-Specialty Clinic', recepetionNodeID: 'specialtyCheckIn', name: 'Multi-Specialty Clinic'},
             {id: 'Center for Integrative Health', recepetionNodeID: 'specialtyCheckIn', name: 'Center for Integrative Health'},
             {id: 'Patient Financial Services', recepetionNodeID: 'specialtyCheckIn', name: 'Patient Financial Services'},
-            {id: 'Radiology', recepetionNodeID: 'specialtyCheckIn', name: 'Radiology'},
+            {id: 'Radiology CH', recepetionNodeID: 'specialtyCheckIn', name: 'Radiology'},
             {id: 'Radiology (MRI/CT Scan)', recepetionNodeID: 'specialtyCheckIn', name: 'Radiology (MRI/CT Scan)'},
             {id: 'Rehabilitation Services', recepetionNodeID: 'specialtyCheckIn', name: 'Rehabilitation Services'}
-        ]
+        ],
+        skipDuplicates: true
     });
+
+    //Patriots Place 22 Floor 4
+    await client.department.createMany({
+        data: [
+            {id: 'Blood Draw/Phlebotomy PP22-4', recepetionNodeID: 'checkIn4000B', name: 'Blood Draw/Phlebotomy'},
+            {id: 'Community Room', recepetionNodeID: 'checkIn4000B', name: 'Community Room'},
+            {id: 'Primary Care', recepetionNodeID: 'checkIn4000B', name: 'Primary Care'}
+        ],
+        skipDuplicates: true
+    });
+
+    //Patriot Place 22 Floor 3
+    await client.department.createMany({
+        data: [
+            {id: 'Multi Specialty Clinic', recepetionNodeID: '3000A', name: 'Multi Specialty Clinic'},
+            {id: 'Allergy', recepetionNodeID: '3000A', name: 'Allergy'},
+            {id: 'Cardiac Arrhythmia', recepetionNodeID: '3000A', name: 'Cardiac Arrhythmia'},
+            {id: 'Dermatology', recepetionNodeID: '3000A', name: 'Dermatology'},
+            {id: 'Endocrinology', recepetionNodeID: '3000A', name: 'Endocrinology'},
+            {id: 'Gastroenterology', recepetionNodeID: '3000A', name: 'Gastroenterology'},
+            {id: 'Kidney (Renal) Medicine', recepetionNodeID: '3000A', name: 'Kidney (Renal) Medicine'},
+            {id: 'Neurology', recepetionNodeID: '3000A', name: 'Neurology'},
+            {id: 'Neurosurgery', recepetionNodeID: '3000A', name: 'Neurosurgery'},
+            {id: 'Opthalmology', recepetionNodeID: '3000A', name: 'Opthalmology'},
+            {id: 'Optometry', recepetionNodeID: '3000A', name: 'Optometry'},
+            {id: 'Pulmonology', recepetionNodeID: '3000A', name: 'Pulmonology'},
+            {id: 'Rhematology', recepetionNodeID: '3000A', name: 'Rhematology'},
+            {id: 'Vein Care Services', recepetionNodeID: '3000A', name: 'Vein Care Services'},
+            {id: "Women's Health", recepetionNodeID: '3000A', name: 'Allergy'},
+            {id: 'Patient Financial Services Fl3', recepetionNodeID: '3000A', name: 'Patient Financial Services'}
+        ],
+        skipDuplicates: true
+    });
+
+    //Patriots Place 20 Floor 1
+    await client.department.createMany({
+        data: [
+            {id: 'Blood Draw/Phlebotomy PP20-1', recepetionNodeID: '130.01', name: 'Blood Draw/Phlebotomy'},
+            {id: 'Pharmacy', recepetionNodeID: '130.01', name: 'Pharmacy'},
+            {id: 'Radiology PP20-1', recepetionNodeID: '110.01', name: 'Radiology'},
+            {id: 'Cardiovascular Services', recepetionNodeID: '120.01', name: 'Cardiovascular Services'},
+            {id: 'Urology', recepetionNodeID: '100.0F', name: 'Urology'},
+            {id: 'Urgent Care Center', recepetionNodeID: '130.01', name: 'Urgent Care Center'}
+        ],
+        skipDuplicates: true
+    });
+
+
+
     console.log('Departments seeded successfully!');
 
 }
