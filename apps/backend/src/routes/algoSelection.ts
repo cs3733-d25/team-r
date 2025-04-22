6616c793dda206fbbc62805c2aa60e58ce616955
sarayu <sarayu.vijayanagarams@gmail.com>
2025-04-22 18:58:26 -0400

    fix: merge changes

--- apps/backend/src/routes/algoSelection.ts
+++ apps/backend/src/routes/algoSelection.ts
@@ -1,34 +1,3 @@
-
-import { Graph } from "./Graph";
-import DFS from "./dfs.ts";
-import { BFS } from "./bfs.ts";
-import { AStar } from "./aStar.ts";
-
-const graph = new Graph();
-
-export async function findPath(
-  start: string,
-  end: string,
-  algorithm: "dfs" | "bfs" | "aStar" = "dfs",
-) {
-  await graph.loadGraph(); // Load graph once
-
-  if (algorithm === "dfs") {
-    const dfs = new DFS(graph);
-    return dfs.findPath(start, end);
-  }
-  if (algorithm === "aStar") {
-    return new AStar(graph).findPath(start, end);
-  }
-  if (algorithm === "bfs") {
-    const bfs = new BFS(graph);
-    return bfs.findPath(start, end);
-  } else {
-    throw new Error(`Algorithm: "${algorithm}" not supported.`);
-  }
-}
-
-
 import { Graph } from "./Graph";
 import { DFS } from "./dfs.ts";
 import { BFS } from "./bfs.ts";
