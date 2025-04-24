d2df6e4d2e31ae062c750a624fe8ac2706319ae5
B-grande <101441595+B-grande@users.noreply.github.com>
2025-04-23 21:59:28 -0400

    added consol logs for dfs file that are similar to bfs

--- apps/backend/src/routes/dfs.ts
+++ apps/backend/src/routes/dfs.ts
@@ -12,20 +12,25 @@ export default class DFS implements PathfindingAlgorithm {
   }
 
   public findPath(start: string, end: string): string[] {
+    console.log("in dfs.pathFind()");
+    console.log(" startingPoint in DFS pathfind: ", start);
+    console.log(" endingPoint in DFS pathfind: ", end);
     const visited = new Set<string>();
     //const stack = new Stack<string[]>();
     this.stack = new Stack<string[]>();
     this.stack.push([start]);
 
+    console.log(" Stack snapshot:");
     while (!this.stack.isEmpty()) {
       const path = this.stack.pop()!;
-      const current = path[path.length - 1];
+      const currentID = path[path.length - 1];
+      
+      if (currentID === end) return path;
 
-      if (current === end) return path;
-
-      if (!visited.has(current)) {
-        visited.add(current);
-        for (const neighbor of this.graph.getNeighbors(current)) {
+      if (!visited.has(currentID)) {
+        visited.add(currentID);
+        for (const neighbor of this.graph.getNeighbors(currentID)) {
+          console.log(`Visiting: ${currentID}`);
           if (!visited.has(neighbor)) {
             this.stack.push([...path, neighbor]);
           }
