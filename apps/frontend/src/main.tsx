c583c06216f139ea832e63c2686a4b54abee23b2
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2025-04-28 19:19:33 -0400

    fix: remove token call for Auth0

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -9,13 +9,13 @@ import {BrowserRouter} from "react-router-dom";
 const domain = import.meta.env.VITE_AUTH_DOMAIN;
 const clientID = import.meta.env.VITE_AUTH_CLIENT_ID
 
-const onRedirectCallback = (appState: any) => {
-    window.history.replaceState(
-        {},
-        document.title,
-        appState?.returnTo || window.location.pathname
-    );
-};
+// const onRedirectCallback = (appState: any) => {
+//     window.history.replaceState(
+//         {},
+//         document.title,
+//         appState?.returnTo || window.location.pathname
+//     );
+// };
 
 // Entry point where root component is rendered into the DOM
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -25,9 +25,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
             authorizationParams={{
                 redirect_uri: window.location.origin + '/external-map',
             }}
-            onRedirectCallback={onRedirectCallback}
-            useRefreshTokens={true}
-            cacheLocation="localstorage"
+            //onRedirectCallback={onRedirectCallback}
+            //cacheLocation="localstorage"
         >
                 <App />
         </Auth0Provider>
