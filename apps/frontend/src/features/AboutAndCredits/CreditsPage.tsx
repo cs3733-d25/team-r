c83d6bc05205c77aa13e45d02dd6b8c6901648e0
Joshua G. <124004980+Joshua-G2@users.noreply.github.com>
2025-04-28 23:23:48 -0400

    add more tools, need pictures

--- apps/frontend/src/features/AboutAndCredits/CreditsPage.tsx
+++ apps/frontend/src/features/AboutAndCredits/CreditsPage.tsx
@@ -4,11 +4,22 @@ import React from 'react'
 
 
 export const CreditsPage = () => {
-
-    const softwareTools = ['PostgresSQL', 'Express', 'React', 'Node.js']
-    const images = ['https://www.postgresql.org/', 'https://expressjs.com/', 'https://react.dev/', 'https://nodejs.org/en']
-
-
+    const devTools = [
+        {name: 'PostgresSQL', link: 'https://www.postgresql.org/', image: ''},
+        {name: 'Express', link: 'https://expressjs.com/', image: ''},
+        {name: 'React', link: 'https://react.dev/', image: 'react.svg'},
+        {name: 'Node.js', link: 'https://nodejs.org/en', image: ''},
+        {name: 'Vite', link: 'https://vite.dev/', image: 'vite.svg'},
+        {name: 'Tailwind', link: 'https://nodejs.org/en', image: ''},
+        {name: 'Shadcn', link: 'https://nodejs.org/en', image: ''},
+        {name: 'Git', link: 'https://nodejs.org/en', image: ''},
+        {name: 'Docker', link: 'https://nodejs.org/en', image: ''},
+        {name: 'Webstorm', link: 'https://nodejs.org/en', image: ''},
+        {name: 'Bruno', link: 'https://nodejs.org/en', image: ''},
+        {name: 'Jira', link: 'https://nodejs.org/en', image: ''},
+        {name: 'AWS', link: 'https://aws.amazon.com/', image: ''},
+    ]
+    
     return (
         <div>
             <h1 className='p-2 text-2xl font-bold font-trade mb-0 place-self-center'>Credits</h1>
@@ -16,14 +27,13 @@ export const CreditsPage = () => {
             <p className='max-w-3xl p-2 text-1xl font-bold font-trade mb-0 place-self-center'> This web application was developed using the PERN (PostgreSQL, Express, React, Node.js) stack with
                 Typescript, Prisma ORM, Shadcn and Tailwind. Deployment on the cloud was accomplished using AWS EC, a
                 Docker container, and AWS RDS to run the PostgreSQL server. </p>
-            <div className='w-full flex flex-wrap justify-center gap-6 p-6'>
-                {softwareTools.map((tool) => (
-                <Card key={tool} className='w-48 h-48'>
-                    <a className='flex-grow flex items-end justify-center' href='https://react.dev/'> <img src="/react.svg" alt={tool} className='w-30 h-30'/> </a>
+            <div className='w-full flex flex-wrap justify-center gap-10 p-10'>
+                {devTools.map((tool)  => (
+                <Card key={tool.name} className='w-48 h-48'>
+                    <a className='flex-grow flex items-end justify-center' href={tool.link}> <img src={tool.image} alt={tool.name} className='w-30 h-30'/> </a>
                     <CardHeader className='justify-center px-0'>
-                        <CardTitle>{tool}</CardTitle>
+                        <CardTitle>{tool.name}</CardTitle>
                     </CardHeader>
-
                 </Card>
             ))}
             </div>
