8419b09667a2b8c2a4c810f7c9d057ce8260b84c
AlexLowczyk <alex.lowczyk@gmail.com>
2025-04-29 19:17:12 -0400

    fix:non emergent patient request has proper tab

--- apps/frontend/src/features/Requests/PatientRequest/AllPatientRequests.tsx
+++ apps/frontend/src/features/Requests/PatientRequest/AllPatientRequests.tsx
@@ -2,42 +2,35 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Table, TableHeader, TableBody, TableHead, TableRow, TableCell} from "@/components/ui/table"
 import {PatientRequestTable} from "@/features/Requests/PatientRequest/PatientRequestTable.tsx";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs.tsx";
+import {DeviceReqForm} from "@/features/Requests/MedDeviceRequest/DeviceReqForm.tsx";
+import PatientRequestForm from "@/features/Requests/PatientRequest/PatientRequestForm.tsx";
+import PatientRequestPage from "@/features/Requests/PatientRequest/PatientRequestPage.tsx";
 
 
 export function AllPatientRequests() {
-    const [patientRequest, setPatientRequest] = useState([{
-        patientRequestID:null,
-        patientID:null,
-        assignedEmpID:null,
-        priority:null,
-        department:null,
-        location:null,
-        comment:null,
-        time:null,
-        status:null,
-        employeeID:null,
-        request: null}]);
-
-    function displayTable() {
-        useEffect(() => {
-            retrieveFromDatabase()
-        }, []);
-    }
-    displayTable();
-    async function retrieveFromDatabase() {
-        try{
-            const response = await axios.get("/api/patientreq/")
-            console.log("response from / get", response.data)
-            setPatientRequest(response.data);
-            console.log(response.data);
-        }
-        catch(error){
-            console.log("error in retrieve:", error);
-        }
-    }
     return(
         <>
-            <PatientRequestTable patientRequest={patientRequest} />
+            <h1 className="text-2xl font-bold font-trade mb-0 place-self-center">Medical Device Request System</h1>
+            <h2 className="text-xl font-bold font-trade mb-6 place-self-center">Nora Cleary & Daksh Gajaria</h2>
+            <Tabs defaultValue="deviceForm">
+                <TabsList>
+                    <TabsTrigger value="patientForm">
+                        Non Emergent Patient Request</TabsTrigger>
+                    <TabsTrigger value="patientPage"
+                    >
+                        View All Requests</TabsTrigger>
+                </TabsList>
+                <div>
+                    <TabsContent value="patientForm">
+                        <PatientRequestForm/>
+                    </TabsContent>
+                    <TabsContent value="patientPage">
+                        <PatientRequestPage/>
+                    </TabsContent>
+                </div>
+            </Tabs>
+
         </>
     )
 }
