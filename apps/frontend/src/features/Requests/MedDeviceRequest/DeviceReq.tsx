3a7ee1632e97c703a7475795f90970513c3dd915
sarayu <sarayu.vijayanagarams@gmail.com>
2025-04-30 19:25:34 -0400

    refactor: remove unnecessary header from DeviceReq for cleaner UI

--- apps/frontend/src/features/Requests/MedDeviceRequest/DeviceReq.tsx
+++ apps/frontend/src/features/Requests/MedDeviceRequest/DeviceReq.tsx
@@ -6,7 +6,6 @@ export function DeviceReq() {
     return(
         <>
             <h1 className="text-2xl font-bold font-trade mb-0 place-self-center">Medical Device Request System</h1>
-            <h2 className="text-xl font-bold font-trade mb-6 place-self-center">Owen Miller & Keagan Hitt & Nora Cleary</h2>
             <Tabs defaultValue="deviceForm">
                 <TabsList>
                     <TabsTrigger value="deviceForm">
