8d53bb74593c094151b592657c74781210ef08f8
sarayu <sarayu.vijayanagarams@gmail.com>
2025-05-02 10:43:42 -0400

    style: add AnnouncementForm route to App component

--- apps/frontend/src/routes/App.tsx
+++ apps/frontend/src/routes/App.tsx
@@ -5,17 +5,13 @@ import Directory from '../features/Directory/Directory.tsx';
 import Login from '../features/Login/Login.tsx';
 import AboutPage from '../features/AboutAndCredits/AboutPage.tsx';
 import CreditsPage from '../features/AboutAndCredits/CreditsPage.tsx';
-import SanitationRequestForm from '../features/Requests/SanitationForm/SanitationRequestForm.tsx';
 import { HomeMain } from '../components/HomeMain.tsx';
 import CSVTabPage from '../features/CSVFiles/CSVTabs.tsx';
 import TestPage from '../features/TestPage.tsx';
 import SanitationRequestPage from "../features/Requests/SanitationForm/SanitationRequestPage.tsx";
 import PatientRequest, { AllPatientRequests } from "../features/Requests/PatientRequest/AllPatientRequests.tsx";
-import { PatientRequestForm } from "@/features/Requests/PatientRequest/PatientRequestForm.tsx";
-import PatientRequestPage from "../features/Requests/PatientRequest/PatientRequestPage.tsx";
 import { ExternalMap } from "../features/MapView/ExternalMap.tsx";
 import { MapPage } from "../features/MapView/MapPage.tsx";
-import PatientTransportPage from "../features/Requests/PatientTransport/PatientTransportPage.tsx";
 import Prescription from "../features/Requests/PrescriptionForm/Prescription.tsx";
 import SanitationRequestTabs from "../features/Requests/SanitationForm/SanitationTabs.tsx";
 import { DeviceReq } from "../features/Requests/MedDeviceRequest/DeviceReq.tsx";
@@ -25,14 +21,13 @@ import { NavbarMGH } from '../components/NavBarMGH/NavbarMGH.tsx';
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import { TourProvider } from '@/components/tour.tsx';
-import TranslateRequestForm from "@/features/Requests/TranslateForm/TranslateRequestForm.tsx";
-import TranslateRequestPage from "@/features/Requests/TranslateForm/TranslateRequestPage.tsx";
 import Translate from "@/features/Requests/TranslateForm/Translate.tsx";
 import PatientTransport from "@/features/Requests/PatientTransport/PatientTransport.tsx";
 import SettingsPage from "@/features/ThemeSwitcher/SettingsPage.tsx"
 import { useTheme } from '../hooks/useTheme';
 import Footer from "../components/Footer";
 import AnnouncementPage from '@/features/Announcements/AnnouncementPage.tsx';
+import AnnouncementForm from "@/features/Announcements/AnnouncementForm.tsx";
 
 
 function App() {
@@ -108,7 +103,8 @@ function App() {
                 { path: 'devicerequest', element: <DeviceReq /> },
                 { path: 'translation', element: <Translate /> },
                 { path: 'requests', element: <TourProvider><RequestPage /></TourProvider> },
-                { path: 'announcements', element: <AnnouncementPage /> }
+                { path: 'announcements', element: <AnnouncementPage /> },
+                { path: 'announcementform', element: <AnnouncementForm /> },
             ],
         },
     ]);
