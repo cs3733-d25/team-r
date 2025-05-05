7217ef0670177d6b8b084d865ccbde54006e0dbb
rmeyers13 <rileyameyers@gmail.com>
2025-05-05 00:35:04 -0400

    Finished patient req dark mode

--- apps/frontend/src/features/Requests/PatientRequest/AllPatientRequests.tsx
+++ apps/frontend/src/features/Requests/PatientRequest/AllPatientRequests.tsx
@@ -9,17 +9,17 @@ export function AllPatientRequests() {
             <h1 className="text-3xl font-bold mb-6 text-center pt-12">Non-Emergent Patient Request</h1>
             <Tabs defaultValue="patientForm">
                 <TabsList>
-                    <TabsTrigger value="patientForm">
+                    <TabsTrigger value="patientForm" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}>
                         Non Emergent Patient Request</TabsTrigger>
-                    <TabsTrigger value="patientPage"
+                    <TabsTrigger value="patientPage" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}
                     >
                         View All Requests</TabsTrigger>
                 </TabsList>
                 <div>
-                    <TabsContent value="patientForm">
+                    <TabsContent value="patientForm" className="dark:bg-background dark:border-gray-600">
                         <PatientRequestForm/>
                     </TabsContent>
-                    <TabsContent value="patientPage">
+                    <TabsContent value="patientPage" className="dark:bg-background dark:border-gray-600">
                         <PatientRequestPage/>
                     </TabsContent>
                 </div>
