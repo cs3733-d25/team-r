b58c826eeaa30cc38d4b0c13570b7a71e2c11011
rmeyers13 <rileyameyers@gmail.com>
2025-05-05 00:33:31 -0400

    Finished sanitation dark mode

--- apps/frontend/src/features/Requests/SanitationForm/SanitationTabs.tsx
+++ apps/frontend/src/features/Requests/SanitationForm/SanitationTabs.tsx
@@ -8,16 +8,16 @@ function SanitationRequestTabs() {
             <h1 className="text-3xl font-bold mb-6 text-center pt-12">Sanitation Request</h1>
             <Tabs defaultValue="sanitationRequestForm" >
                 <TabsList>
-                    <TabsTrigger value="sanitationRequestForm">
+                    <TabsTrigger value="sanitationRequestForm" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}>
                         Sanitation Request Form</TabsTrigger>
-                    <TabsTrigger value="sanitationRequestTable">
+                    <TabsTrigger value="sanitationRequestTable" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}>
                         View All Requests</TabsTrigger>
                 </TabsList>
                 <div >
-                    <TabsContent value="sanitationRequestForm">
+                    <TabsContent value="sanitationRequestForm" className="dark:bg-background dark:border-gray-600">
                         <SanitationRequestForm/>
                     </TabsContent>
-                    <TabsContent value="sanitationRequestTable">
+                    <TabsContent value="sanitationRequestTable" className="dark:bg-background dark:border-gray-600">
                         <SanitationRequestPage />
                     </TabsContent>
                 </div>
