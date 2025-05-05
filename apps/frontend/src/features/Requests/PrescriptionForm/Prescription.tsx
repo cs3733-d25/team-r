cbba3f01fedc4c2fb20e447f2303e356f08f8a28
rmeyers13 <rileyameyers@gmail.com>
2025-05-05 00:37:27 -0400

    Finished pharmacy dark mode

--- apps/frontend/src/features/Requests/PrescriptionForm/Prescription.tsx
+++ apps/frontend/src/features/Requests/PrescriptionForm/Prescription.tsx
@@ -8,18 +8,18 @@ export function Prescription() {
             <h1 className="text-3xl font-bold mb-6 text-center pt-12">Prescription Request</h1>
             <Tabs defaultValue="prescriptionForm">
                 <TabsList>
-                    <TabsTrigger value="prescriptionForm"
+                    <TabsTrigger value="prescriptionForm" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}
                                  >
                         Prescription Request</TabsTrigger>
-                    <TabsTrigger value="prescriptionPage"
+                    <TabsTrigger value="prescriptionPage" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}
                                  >
                         View All Requests</TabsTrigger>
                 </TabsList>
                 <div>
-                    <TabsContent value="prescriptionForm">
+                    <TabsContent value="prescriptionForm" className="dark:bg-background dark:border-gray-600">
                         <PrescriptionForm/>
                     </TabsContent>
-                    <TabsContent value="prescriptionPage">
+                    <TabsContent value="prescriptionPage" className="dark:bg-background dark:border-gray-600">
                         <PrescriptionPage />
                     </TabsContent>
             </div>
