a19fee01d6705d2fc0d8b80e81e96606746b3da9
rmeyers13 <rileyameyers@gmail.com>
2025-05-05 00:30:59 -0400

    Finished medical device dark mode

--- apps/frontend/src/features/Requests/MedDeviceRequest/DeviceReq.tsx
+++ apps/frontend/src/features/Requests/MedDeviceRequest/DeviceReq.tsx
@@ -8,17 +8,17 @@ export function DeviceReq() {
             <h1 className="text-3xl font-bold mb-6 text-center pt-12">Medical Device Request</h1>
             <Tabs defaultValue="deviceForm">
                 <TabsList>
-                    <TabsTrigger value="deviceForm">
+                    <TabsTrigger value="deviceForm" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}>
                         Medical Device Request</TabsTrigger>
-                    <TabsTrigger value="devicePage"
+                    <TabsTrigger value="devicePage" className={"border border-gray-300 dark:border-gray-600 data-[state=active]:bg-white dark:data-[state=active]:bg-gray-800 data-[state=active]:text-black dark:data-[state=active]:text-white dark:text-gray-300"}
                                  >
                         View All Requests</TabsTrigger>
                 </TabsList>
                 <div>
-                    <TabsContent value="deviceForm">
+                    <TabsContent value="deviceForm" className="dark:bg-background dark:border-gray-600">
                         <DeviceReqForm/>
                     </TabsContent>
-                    <TabsContent value="devicePage">
+                    <TabsContent value="devicePage" className="dark:bg-background dark:border-gray-600">
                         <DeviceReqPage />
                     </TabsContent>
                 </div>
