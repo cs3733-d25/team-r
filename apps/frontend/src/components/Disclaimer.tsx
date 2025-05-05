6864565196643fb24468c6a1d3486f26f1d05342
Keagan Hitt <kjhitt@wpi.edu>
2025-05-05 09:25:01 -0400

    bugfix: can no longer X out of disclaimer

--- apps/frontend/src/components/Disclaimer.tsx
+++ apps/frontend/src/components/Disclaimer.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ExclamationTriangle, X } from 'react-bootstrap-icons';
+import { ExclamationTriangle } from 'react-bootstrap-icons';
 
 //Input any message for the disclaimer when it is called so it can be reused.
 interface DisclaimerPopupProps {
@@ -7,12 +7,6 @@ interface DisclaimerPopupProps {
 }
 
 const DisclaimerPopup: React.FC<DisclaimerPopupProps> = ({ message }) => {
-    const [isVisible, setIsVisible] = useState(true);
-
-    if (!isVisible) {
-        return null; // Don't render the popup if it's not visible
-    }
-
     return (
         <div className="fixed top-20 inset-x-0 mx-auto bg-white border border-gray-300 rounded-xl shadow-lg p-4 flex items-center w-1/2 z-50">
             <div className="text-red-600 mr-3 animate-flash">
@@ -22,7 +16,6 @@ const DisclaimerPopup: React.FC<DisclaimerPopupProps> = ({ message }) => {
             <div>
                 <div className="flex items-center justify-between mb-2">
                     <p className="font-bold text-red-600">Disclaimer:</p>
-                    <X className="relative -top-1 text-2xl cursor-pointer" onClick={() => setIsVisible(false)} />
                 </div>
                 <p className="text-sm text-gray-800">{message}</p>
             </div>
